79cb545bca815ba03b345ec244523461264c7535
Itira <yaninilya0501@gmail.com>
2025-08-09 13:54:09 +0200

    Типы данных

---
+++ August/lesson1.py
@@ -0,0 +1,35 @@
+#Типы данных
+
+#Целые числа / integer / int
+my_int = 5
+print(type(my_int))
+
+# Дробные числа / Вещественные числа / Числа с плавающей точкой / float
+my_float = 7.43
+print(type(my_float))
+
+#Строки / string / str
+my_str_1 = 'Hello'
+my_str_2 = "Hello"
+print(type(my_str_1))
+
+#Списки / list
+my_list = ['Irina', 52, 181.5]
+print(type(my_list))
+
+#Картеж / tuple
+my_tuple = ('password', 'username')
+print(type(my_tuple))
+
+#Множества / set
+my_set = {1, 2, 3, 1, 2, 3, 1, 2, 3}
+print(type(my_set))
+
+#Словари / Dictionary / dict
+my_dict = {'name':'Irina', 'age': 52, 'height': 181.5}
+print(type(my_dict))
+
+#Логический тип / boolean / bool
+my_bool_1 = True
+my_bool_2 = False
+print(type(my_bool_1))
